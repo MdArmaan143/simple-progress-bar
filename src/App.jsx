4d94ae97091e4eef1ef4b5e5b25609a8452099e7
Md Armaan <armaanshaikhh786@gmail.com>
2024-09-03 14:24:52 +0530

    Progress Bar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import ProgressBar from './framer-components/ProgressBar'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='p-10 flex flex-col'>
+        <h1 className='text-center text-4xl font-semibold'>
+          Framer Motion Components
+        </h1>
+        <ProgressBar/>
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
